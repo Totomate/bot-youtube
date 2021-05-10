4957e4781b531b9c75400f22e735317eafe9aec7
Totomatee <77230901+Totomatee@users.noreply.github.com>
2021-05-10 17:34:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require("node-fetch");
+
+const config = {
+    token: "YOUR_TOKEN",
+    prefix: "YOUR_PREFIX"
+}
+
+client.on('ready', () => {
+
+ 
+    client.user.setActivity(`${config.prefix}youtube`);
+    console.log(`${client.user.username} : ${prefix} ✅`)
+});
+
+client.on('message', message => {
+	if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if(command === 'youtube') {
+	
+	
+	 const channel =  message.guild.channels.cache.get(args[0]);
+            if (!channel || channel.type !== "voice") return message.channel.send(`❌ | Salon spécifié invalide!`);
+            if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.channel.send("❌ | J'ai besoin de la permission `CREER UNE INVITATION`");
+    
+	
+	
+	
+		 fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+                method: "POST",
+                body: JSON.stringify({
+                    max_age: 86400,
+                    max_uses: 0,
+                    target_application_id: "755600276941176913", // youtube together
+                    target_type: 2,
+                    temporary: false,
+                    validate: null
+                }),
+                headers: {
+                    "Authorization": `Bot ${client.token}`,
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(res => res.json())
+                .then(invite => {
+                    if (invite.error || !invite.code) return message.channel.send(`❌ | Erreur, je ne peux pas commencer l'activité **YouTube**!`);
+                    message.channel.send(`✅ | Clique ici pour commencer **YouTube** ${language(guild, 'in')} ${channel.name}: <https://discord.gg/${invite.code}>`);
+                })
+                .catch(e => {
+                    message.channel.send(`❌ | Erreur, je ne peux pas commencer l'activité **YouTube**!`);
+                })
+	} else if(command === 'dev' || command === 'developper') {
+		return message.channel.send("From totomate")
+	} 
+});
+
+client.login(config.token);
